4eccb32aa4b7a9fef8346f5f7efb10d618e46a51
kushwahji <santoshkumar021990@gmail.com>
2022-07-25 13:50:09 +0530

    cdss

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const path = require('path');
 
-const app = express();
+const path = require('path');   
 
+const app = express();   
 
-app.use(express.static(__dirname + '/dist/heroku-deploy'));
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/dist/heroku-deploy/index.html'));
-});
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
 
+// Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
