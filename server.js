43eed88c1aedd8a52e53e0deb324b467a52f082c
kushwahji <santoshkumar021990@gmail.com>
2022-07-25 18:46:33 +0530

    cdss

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/angular-blog'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/askmequestions/'}),
+    res.sendFile('index.html', {root: 'dist/angular-blog/'}),
 );
 
 // Start the app by listening on the default Heroku port
