068e4e5cc992c9f9b06eb6ebe794b47064b04a11
kushwahji <santoshkumar021990@gmail.com>
2022-07-25 12:28:25 +0530

    add new

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-/Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
+app.use(express.static(__dirname + '/dist/heroku-deploy'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 9999);
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/dist/heroku-deploy/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
